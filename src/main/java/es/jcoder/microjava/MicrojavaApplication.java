b2cc0864b9ece48e737d9c3c1c36829e6fe69fb0
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2023-03-04 12:41:25 +0100

    First Commit

---
+++ src/main/java/es/jcoder/microjava/MicrojavaApplication.java
@@ -0,0 +1,13 @@
+package es.jcoder.microjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/jcoder/microjava/MicrojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jcoder.microjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
